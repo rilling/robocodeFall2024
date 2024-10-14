baa4c5afd854e091bc09a167e3698fc4c435337c
Yu-hang Lin <linyuhang617@gmail.com>
2024-10-14 14:01:45 -0400

    Add a private constructor to hide the implicit public one.

--- robocode.api/src/main/java/net/sf/robocode/io/FileUtil.java
+++ robocode.api/src/main/java/net/sf/robocode/io/FileUtil.java
@@ -29,11 +29,6 @@ public class FileUtil {
 	// Current working directory
 	private static File cwd;
 
-	// ** Change made here: Adding a private constructor **
-	private FileUtil() {
-		// Prevent instantiation
-	}
-
 	// Initializes the current working directory
 	static {
 		try {
