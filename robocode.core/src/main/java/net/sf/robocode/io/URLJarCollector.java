d13c134637774f6ebdc5c2d9c28b816b142c456c
SreePravallika15 <sreepravallika4410@gmail.com>
2024-12-02 14:13:43 -0500

    Fixed PrepareBattles.java

--- robocode.core/src/main/java/net/sf/robocode/io/URLJarCollector.java
+++ robocode.core/src/main/java/net/sf/robocode/io/URLJarCollector.java
@@ -7,200 +7,167 @@
  */
 package net.sf.robocode.io;
 
+
 import java.net.URLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.net.InetAddress;
 import java.lang.reflect.Field;
 import java.util.*;
 import java.util.jar.JarFile;
 import java.io.File;
 import java.io.IOException;
 
+
 /**
- * This class helps with closing of robot .jar files when used with URL, URLConnection, and useCaches=true.
+ * This class is helping with closing of robot .jar files when used with URL, URLConnection and useCaches=true
  * It is designed to close JarFiles opened and cached in SUN's JarFileFactory.
+ * If we are not on SUN's JVM, we fall back to useCaches=false, to not lock the files.
+ * Collection is now called after repository refresh and after battle ended.
+ * Collection is disabled/postponed during running battle.
  * 
- * @author Pavel Savara
- * @author Flemming N. Larsen
+ * @author Pavel Savara (original)
+ * @author Flemming N. Larsen (contributor)
  */
 public class URLJarCollector {
+	
+	private static HashMap<?, ?> fileCache;
+	private static HashMap<?, ?> urlCache;
+	private static Field jarFileURL;
+	private static final boolean sunJVM;
+	private static boolean enabled;
+	private static final Set<URL> urlsToClean;
 
-    private static HashMap<?, ?> fileCache;
-    private static HashMap<?, ?> urlCache;
-    private static Field jarFileURL;
-    private static final boolean sunJVM;
-    private static boolean enabled;
-    private static final Set<URL> urlsToClean;
+	static {
+		boolean localSunJVM = false;
 
-    static {
-        boolean localSunJVM = false;
+		try {
+			final Class<?> jarFactory = ClassLoader.getSystemClassLoader().loadClass(
+					"sun.net.www.protocol.jar.JarFileFactory");
+			final Field fileCacheF = jarFactory.getDeclaredField("fileCache");
 
-        try {
-            final Class<?> jarFactory = ClassLoader.getSystemClassLoader().loadClass(
-                    "sun.net.www.protocol.jar.JarFileFactory");
-            final Field fileCacheF = jarFactory.getDeclaredField("fileCache");
+			fileCacheF.setAccessible(true);
+			fileCache = (HashMap<?, ?>) fileCacheF.get(null);
 
-            fileCacheF.setAccessible(true);
-            fileCache = (HashMap<?, ?>) fileCacheF.get(null);
+			final Field urlCacheF = jarFactory.getDeclaredField("urlCache");
 
-            final Field urlCacheF = jarFactory.getDeclaredField("urlCache");
+			urlCacheF.setAccessible(true);
+			urlCache = (HashMap<?, ?>) urlCacheF.get(null);
 
-            urlCacheF.setAccessible(true);
-            urlCache = (HashMap<?, ?>) urlCacheF.get(null);
+			final Class<?> jarURLConnection = ClassLoader.getSystemClassLoader().loadClass(
+					"sun.net.www.protocol.jar.JarURLConnection");
 
-            final Class<?> jarURLConnection = ClassLoader.getSystemClassLoader().loadClass(
-                    "sun.net.www.protocol.jar.JarURLConnection");
+			jarFileURL = jarURLConnection.getDeclaredField("jarFileURL");
+			jarFileURL.setAccessible(true);
 
-            jarFileURL = jarURLConnection.getDeclaredField("jarFileURL");
-            jarFileURL.setAccessible(true);
+			localSunJVM = true;
+		} catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException ignore) {
+		}
+		sunJVM = localSunJVM;
 
-            localSunJVM = true;
-        } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException ignore) {
-        }
-        sunJVM = localSunJVM;
+		urlsToClean = Collections.newSetFromMap(new WeakHashMap<>());
+	}
 
-        urlsToClean = Collections.newSetFromMap(new WeakHashMap<>());
-    }
+	public static synchronized URLConnection openConnection(URL url) throws IOException {
+		// Logger.logMessage("Open connection to URL: " + url);
+		final URLConnection urlConnection = url.openConnection();
 
-    public static synchronized URLConnection openConnection(URL url) throws IOException {
-        validateURL(url); // Add URL validation to prevent SSRF
+		if (sunJVM) {
+			registerConnection(urlConnection);
+			urlConnection.setUseCaches(true);
+		} else {
+			urlConnection.setUseCaches(false);
+		}
+		return urlConnection;
+	}
 
-        final URLConnection urlConnection = url.openConnection();
+	public static synchronized void enableGc(boolean enabled) {
+		URLJarCollector.enabled = enabled;
+	}
 
-        if (sunJVM) {
-            registerConnection(urlConnection);
-            urlConnection.setUseCaches(true);
-        } else {
-            urlConnection.setUseCaches(false);
-        }
-        return urlConnection;
-    }
+	public static synchronized void gc() {
+		if (sunJVM) {
+			// Close all JarURLConnections if garbage collection is enabled
+			if (enabled) {
+				synchronized (urlsToClean) {
+					for (URL url : urlsToClean) {
+						closeJarURLConnection(url);
+					}
+					urlsToClean.clear();
+				}
+			}
 
-    public static synchronized void enableGc(boolean enabled) {
-        URLJarCollector.enabled = enabled;
-    }
+			// Bug fix [2867326] - Lockup on start if too many bots in robots dir (cont'd).
 
-    public static synchronized void gc() {
-        if (sunJVM) {
-            if (enabled) {
-                synchronized (urlsToClean) {
-                    for (URL url : urlsToClean) {
-                        closeJarURLConnection(url);
-                    }
-                    urlsToClean.clear();
-                }
-            }
+			// Remove all cache entries to temporary jar cache files created
+			// for connections using the jarjar protocol that get stuck up.
+			for (Iterator<?> it = fileCache.keySet().iterator(); it.hasNext();) {
+				Object urlJarFile = it.next();
 
-            for (Iterator<?> it = fileCache.keySet().iterator(); it.hasNext();) {
-                Object urlJarFile = it.next();
+				final JarFile jarFile = (JarFile) fileCache.get(urlJarFile);
 
-                final JarFile jarFile = (JarFile) fileCache.get(urlJarFile);
+				String filename = jarFile.getName();
 
-                String filename = jarFile.getName();
-                filename = filename.substring(filename.lastIndexOf(File.separatorChar) + 1).toLowerCase();
+				filename = filename.substring(filename.lastIndexOf(File.separatorChar) + 1).toLowerCase();
 
-                if (filename.startsWith("jar_cache")) {
-                    it.remove();
-                    synchronized (urlCache) {
-                        urlCache.remove(jarFile);
-                    }
-                }
-            }
-        }
-    }
+				if (filename.startsWith("jar_cache")) {
+					it.remove();
+					synchronized (urlCache) {
+						urlCache.remove(jarFile);
+					}
+				}
+			}
+		}
+	}
 
-    private static void registerConnection(URLConnection conn) {
-        if (conn != null) {
-            final String cl = conn.getClass().getName();
+	private static void registerConnection(URLConnection conn) {
+		if (conn != null) {
+			final String cl = conn.getClass().getName();
 
-            if (cl.equals("sun.net.www.protocol.jar.JarURLConnection")) {
-                try {
-                    final URL url = (URL) jarFileURL.get(conn);
+			if (cl.equals("sun.net.www.protocol.jar.JarURLConnection")) {
+				try {
+					final URL url = (URL) jarFileURL.get(conn);
 
-                    if (!urlsToClean.contains(url)) {
-                        synchronized (urlsToClean) {
-                            urlsToClean.add(url);
-                        }
-                    }
-                } catch (IllegalAccessException ignore) {}
-            }
-        }
-    }
+					if (!urlsToClean.contains(url)) {
+						synchronized (urlsToClean) {
+							urlsToClean.add(url);
+						}
+					}
+				} catch (IllegalAccessException ignore) {}
+			}
+		}
+	}
 
-    public synchronized static void closeJarURLConnection(URL url) {
-        if (url != null) {
-            for (Iterator<?> it = fileCache.keySet().iterator(); it.hasNext();) {
-                Object urlJarFile = it.next();
+	// Added due to bug fix [2867326] - Lockup on start if too many bots in robots dir (cont'd).
+	public synchronized static void closeJarURLConnection(URL url) {
+		if (url != null) {
+			for (Iterator<?> it = fileCache.keySet().iterator(); it.hasNext();) {
+				Object urlJarFile = it.next();
 
-                final JarFile jarFile = (JarFile) fileCache.get(urlJarFile);
+				final JarFile jarFile = (JarFile) fileCache.get(urlJarFile);
 
-                String urlPath = url.getPath();
+				String urlPath = url.getPath();
 
-                try {
-                    urlPath = URLDecoder.decode(urlPath, "UTF-8");
-                } catch (java.io.UnsupportedEncodingException ignore) {}
+				try {
+					urlPath = URLDecoder.decode(urlPath, "UTF-8");
+				} catch (java.io.UnsupportedEncodingException ignore) {}
 
-                File urlFile = new File(urlPath);
+				File urlFile = new File(urlPath);
 
-                String jarFileName = jarFile.getName();
-                String urlFileName = urlFile.getPath();
+				String jarFileName = jarFile.getName();
+				String urlFileName = urlFile.getPath();
 
-                if (urlFileName.equals(jarFileName)) {
-                    it.remove();
-                    synchronized (urlCache) {
-                        urlCache.remove(jarFile);
-                    }
-                    try {
-                        jarFile.close();
-                    } catch (IOException e) {
-                        Logger.logError(e);
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Validates a given URL to mitigate SSRF vulnerabilities.
-     * @param url The URL to validate.
-     * @throws IOException If the URL is invalid or unsafe.
-     */
-    private static void validateURL(URL url) throws IOException {
-        if (url == null) {
-            throw new IOException("URL cannot be null");
-        }
-
-        String protocol = url.getProtocol();
-        if (!"http".equalsIgnoreCase(protocol) && !"https".equalsIgnoreCase(protocol)) {
-            throw new IOException("Unsupported protocol: " + protocol);
-        }
-
-        String host = url.getHost();
-        if (host == null || host.isEmpty()) {
-            throw new IOException("URL must have a valid host");
-        }
-
-        if (host.equalsIgnoreCase("localhost") || host.equals("127.0.0.1")) {
-            throw new IOException("Access to localhost is prohibited");
-        }
-
-        if (isPrivateIPAddress(host)) {
-            throw new IOException("Access to private IP addresses is prohibited");
-        }
-    }
-
-    /**
-     * Checks if a given host is a private IP address.
-     * @param host The host to check.
-     * @return true if the host is a private IP address; false otherwise.
-     */
-    private static boolean isPrivateIPAddress(String host) {
-        try {
-            InetAddress inetAddress = InetAddress.getByName(host);
-            return inetAddress.isSiteLocalAddress();
-        } catch (Exception e) {
-            return false;
-        }
-    }
+				if (urlFileName.equals(jarFileName)) {
+					it.remove();
+					synchronized (urlCache) {
+						urlCache.remove(jarFile);
+					}
+					try {
+						jarFile.close();
+					} catch (IOException e) {
+						Logger.logError(e);
+					}
+				}
+			}
+		}
+	}
 }
